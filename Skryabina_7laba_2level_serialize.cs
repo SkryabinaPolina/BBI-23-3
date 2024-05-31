092e484f70e66a9559ab48775a876afb6d3c60f4
SkryabinaPolina <159529989+SkryabinaPolina@users.noreply.github.com>
2024-05-31 16:46:36 +0300

    9 laba

--- Skryabina_7laba_2level_serialize.cs
+++ Skryabina_7laba_2level_serialize.cs
@@ -76,177 +76,258 @@ internal class Program
             Console.WriteLine(ex.Message);
         }
     }
-    public class Quest
+}
+public class Quest
+{
+    private readonly string[][] _questions;
+    private int _currIndex;
+
+    public Quest(int maxQuestions = 100)
     {
-        private readonly string[][] _questions;
-        private int _currIndex;
-
-        public Quest(int maxQuestions = 100)
+        _questions = new string[3][];
+        for (int i = 0; i < 3; i++)
         {
-            _questions = new string[3][];
-            for (int i = 0; i < 3; i++)
+            _questions[i] = new string[maxQuestions];
+        }
+        _currIndex = 0;
+    }
+
+    public void Append(string[] answers)
+    {
+        if (answers.Length != 3)
+        {
+            throw new Exception("Должно быть 3 ответа");
+        }
+        for (int i = 0; i < answers.Length; i++)
+        {
+            _questions[i][_currIndex] = answers[i];
+        }
+        _currIndex++;
+    }
+
+    public AnswerData[] GetTop(int iQuestion)
+    {
+        AnswerData[] data = new AnswerData[_questions[iQuestion].Length];
+        int dataIndex = 0;
+        string[] currQuestions = _questions[iQuestion];
+        var query = currQuestions.OrderBy(x => x);
+
+        string currQuestion = query.First();
+        int currCnt = 0;
+        int nullCnt = 0;
+        foreach (string item in query)
+        {
+            if (item == "")
             {
-                _questions[i] = new string[maxQuestions];
+                nullCnt++;
             }
-            _currIndex = 0;
+            if (item == currQuestion)
+            {
+                currCnt++;
+            }
+            else
+            {
+                if (currQuestion != "")
+                {
+                    data[dataIndex] = new AnswerData(currQuestion, currCnt / (double)(_questions[iQuestion].Length - nullCnt) * 100);
+                    dataIndex++;
+                }
+
+                currCnt = 1;
+                currQuestion = item;
+            }
         }
 
-        public void Append(string[] answers)
-        {
-            if (answers.Length != 3)
-            {
-                throw new Exception("Должно быть 3 ответа");
-            }
-            for (int i = 0; i < answers.Length; i++)
-            {
-                _questions[i][_currIndex] = answers[i];
-            }
-            _currIndex++;
-        }
+        data[dataIndex] = new AnswerData(currQuestion, currCnt / (double)(_questions[iQuestion].Length - nullCnt) * 100);
+        dataIndex++;
 
-        public AnswerData[] GetTop(int iQuestion)
-        {
-            AnswerData[] data = new AnswerData[_questions[iQuestion].Length];
-            int dataIndex = 0;
-            string[] currQuestions = _questions[iQuestion];
-            var query = currQuestions.OrderBy(x => x);
+        return data;
+    }
 
-            string currQuestion = query.First();
-            int currCnt = 0;
-            int nullCnt = 0;
-            foreach (string item in query)
+    public List<string> Show()
+    {
+        var showResult = new List<string>();
+        for (int i = 0; i < 3; i++)
+        {
+            AnswerData[] data = GetTop(i);
+            showResult.Add($"Вопрос #{i + 1}\n---");
+            int cnt = 0;
+            for (int j = 0; j < data.Length; j++)
             {
-                if (item == "")
+                if (data[j] != null)
                 {
-                    nullCnt++;
-                }
-                if (item == currQuestion)
-                {
-                    currCnt++;
-                }
-                else
-                {
-                    if (currQuestion != "")
+                    showResult.Add($"{data[j].Percentage,4:f1}% : {data[j].Answer}");
+                    cnt++;
+                    if (cnt >= 5)
                     {
-                        data[dataIndex] = new AnswerData(currQuestion, currCnt / (double)(_questions[iQuestion].Length - nullCnt) * 100);
-                        dataIndex++;
-                    }
-
-                    currCnt = 1;
-                    currQuestion = item;
-                }
-            }
-
-            data[dataIndex] = new AnswerData(currQuestion, currCnt / (double)(_questions[iQuestion].Length - nullCnt) * 100);
-            dataIndex++;
-
-            return data;
-        }
-
-        public List<string> Show()
-        {
-            var showResult = new List<string>();
-            for (int i = 0; i < 3; i++)
-            {
-                AnswerData[] data = GetTop(i);
-                showResult.Add($"Вопрос #{i + 1}\n---");
-                int cnt = 0;
-                for (int j = 0; j < data.Length; j++)
-                {
-                    if (data[j] != null)
-                    {
-                        showResult.Add($"{data[j].Percentage,4:f1}% : {data[j].Answer}");
-                        cnt++;
-                        if (cnt >= 5)
-                        {
-                            break;
-                        }
+                        break;
                     }
                 }
-                showResult.Add("-----");
             }
-            return showResult;
+            showResult.Add("-----");
         }
+        return showResult;
+    }
+}
+
+public class AnswerData
+{
+    public string Answer { get; }
+    public double Percentage { get; }
+
+    public AnswerData(string answer, double percentage = 0)
+    {
+        Answer = answer;
+        Percentage = percentage;
+    }
+}
+
+public abstract class Country
+{
+    public abstract List<string> ShowResults();
+}
+
+public class Russia : Country
+{
+    private readonly Quest _quest;
+
+    public Russia(Quest quest)
+    {
+        _quest = quest;
     }
 
-    public class AnswerData
+    public override List<string> ShowResults()
     {
-        public string Answer { get; }
-        public double Percentage { get; }
-
-        public AnswerData(string answer, double percentage = 0)
-        {
-            Answer = answer;
-            Percentage = percentage;
-        }
-    }
-
-    public abstract class Country
-    {
-        public abstract List<string> ShowResults();
-    }
-
-    public class Russia : Country
-    {
-        private readonly Quest _quest;
-
-        public Russia(Quest quest)
-        {
-            _quest = quest;
-        }
-
-        public override List<string> ShowResults()
-        {
-            var showResult = new List<string>()
+        var showResult = new List<string>()
         {
             "Результаты для России:"
         };
-            var questsShow = _quest.Show();
-            foreach (var questShow in questsShow)
-            {
-                showResult.Add(questShow);
-            }
-            return showResult;
+        var questsShow = _quest.Show();
+        foreach (var questShow in questsShow)
+        {
+            showResult.Add(questShow);
         }
+        return showResult;
+    }
+}
+
+public class Japan : Country
+{
+    private readonly Quest _quest;
+
+    public Japan(Quest quest)
+    {
+        _quest = quest;
     }
 
-    public class Japan : Country
+    public override List<string> ShowResults()
     {
-        private readonly Quest _quest;
-
-        public Japan(Quest quest)
-        {
-            _quest = quest;
-        }
-
-        public override List<string> ShowResults()
-        {
-            var showResult = new List<string>()
+        var showResult = new List<string>()
         {
             "Результаты для Японии:"
         };
-            var questsShow = _quest.Show();
-            foreach (var questShow in questsShow)
-            {
-                showResult.Add(questShow);
-            }
-            return showResult;
-        }
-
-        public class Result
+        var questsShow = _quest.Show();
+        foreach (var questShow in questsShow)
         {
-            public string Description { get; set; }
-            public Result()
-            {
+            showResult.Add(questShow);
+        }
+        return showResult;
+    }
 
+    public class Result
+    {
+        public string Description { get; set; }
+        public Result()
+        {
+
+        }
+    }
+
+    public abstract class Serializer
+    {
+        protected Serializer()
+        {
+
+        }
+        public abstract void Serialize(IList<Result> results, string fileName);
+        public abstract IList<Result> Deserialize(string fileName);
+    }
+
+    public class MyJsonSerializer : Serializer
+    {
+        public MyJsonSerializer()
+        {
+
+        }
+
+        public override IList<Result> Deserialize(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return JsonSerializer.Deserialize<IList<Result>>(File.ReadAllText(fileName));
+            }
+            else
+            {
+                throw new Exception("Файл не найден!");
             }
         }
 
+        public override void Serialize(IList<Result> results, string fileName)
+        {
+            try
+            {
+                File.WriteAllText(fileName, JsonSerializer.Serialize(results));
+            }
+            catch
+            {
+                throw new Exception("Запись в файл не удалась");
+            }
+        }
+    }
+
+    public class MyXmlSerializer : Serializer
+    {
+        public MyXmlSerializer()
+        {
+
+        }
+
+        public override IList<Result> Deserialize(string fileName)
+        {
+            try
+            {
+                using (var stream = new FileStream(fileName, FileMode.OpenOrCreate))
+                {
+                    var xml = new XmlSerializer(typeof(IList<Result>));
+                    return xml.Deserialize(stream) as List<Result>;
+                }
+            }
+            catch
+            {
+                throw new Exception("Запись в файл не удалась");
+            }
+        }
+
+        public override void Serialize(IList<Result> results, string fileName)
+        {
+            try
+            {
+                using (var stream = new FileStream(fileName, FileMode.OpenOrCreate))
+                {
+                    var xml = new XmlSerializer(typeof(IList<Result>));
+                    xml.Serialize(stream, results);
+                }
+            }
+            catch
+            {
+                throw new Exception("Запись в файл не удалась");
+            }
+        }
         public abstract class Serializer
         {
-            protected Serializer() 
+            protected Serializer()
             {
-                
+
             }
             public abstract void Serialize(IList<Result> results, string fileName);
             public abstract IList<Result> Deserialize(string fileName);
@@ -256,87 +337,7 @@ internal class Program
         {
             public MyJsonSerializer()
             {
-                
-            }
 
-            public override IList<Result> Deserialize(string fileName)
-            {
-                if (File.Exists(fileName))
-                {
-                    return JsonSerializer.Deserialize<IList<Result>>(File.ReadAllText(fileName));
-                }
-                else
-                {
-                    throw new Exception("Файл не найден!");
-                }
-            }
-
-            public override void Serialize(IList<Result> results, string fileName)
-            {
-                try
-                {
-                    File.WriteAllText(fileName, JsonSerializer.Serialize(results));
-                }
-                catch
-                {
-                    throw new Exception("Запись в файл не удалась");
-                }
-            }
-        }
-
-        public class MyXmlSerializer : Serializer
-        {
-            public MyXmlSerializer()
-            {
-
-            }
-
-            public override IList<Result> Deserialize(string fileName)
-            {
-                try
-                {
-                    using (var stream = new FileStream(fileName, FileMode.OpenOrCreate))
-                    {
-                        var xml = new XmlSerializer(typeof(IList<Result>));
-                        return xml.Deserialize(stream) as List<Result>;
-                    }
-                }
-                catch
-                {
-                    throw new Exception("Запись в файл не удалась");
-                }
-            }
-
-            public override void Serialize(IList<Result> results, string fileName)
-            {
-                try
-                {
-                    using (var stream = new FileStream(fileName, FileMode.OpenOrCreate))
-                    {
-                        var xml = new XmlSerializer(typeof(IList<Result>));
-                        xml.Serialize(stream, results);
-                    }
-                }
-                catch
-                {
-                    throw new Exception("Запись в файл не удалась");
-                }
-            }
-			public abstract class Serializer
-        {
-            protected Serializer() 
-            {
-                
-            }
-            public abstract void Serialize(IList<Result> results, string fileName);
-            public abstract IList<Result> Deserialize(string fileName);
-        }
-
-        public class MyJsonSerializer : Serializer
-        {
-            public MyJsonSerializer()
-            {
-                
             }
 
             public override IList<Result> Deserialize(string fileName)
@@ -404,4 +405,3 @@ internal class Program
             }
         }
     }
-}
